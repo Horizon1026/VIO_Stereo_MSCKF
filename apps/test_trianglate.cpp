2ed2384bfd32bc8f435239541d8a86779fd21d75
Horizon1026 <lijiapeng_atlantis@163.com>
2022-10-29 13:36:47 +0800

    multi-view vio

--- apps/test_trianglate.cpp
+++ apps/test_trianglate.cpp
@@ -45,8 +45,8 @@ bool TestTrianglateIterative() {
     uint32_t poseNums = 6;      // 相机数目
     Vector3 p_w{2, 2, 2};
     std::vector<Vector2> observe_vec;
-    std::vector<Quaternion> q_cw_vec;
-    std::vector<Vector3> p_cw_vec;
+    std::vector<Quaternion> q_wc_vec;
+    std::vector<Vector3> p_wc_vec;
 
     Scalar radius = 8;
     for (uint32_t n = 0; n < poseNums; ++n) {
@@ -57,14 +57,14 @@ bool TestTrianglateIterative() {
         Vector3 p_cw = Vector3(radius * cos(theta) - radius, radius * sin(theta), 1 * sin(2 * theta));
         auto p_c = R_cw * p_w + p_cw;
         observe_vec.emplace_back(p_c[0] / p_c[2], p_c[1] / p_c[2]);
-        q_cw_vec.emplace_back(R_cw);
-        p_cw_vec.emplace_back(p_cw);
+        q_wc_vec.emplace_back(R_cw.transpose());
+        p_wc_vec.emplace_back(- R_cw.transpose() * p_cw);
     }
 
     Vector3 p_w_noise = Vector3(0.4, 0.4, 0.4);
     Vector3 res_p_w = p_w + p_w_noise;
 
-    solver.TrianglateIterative(q_cw_vec, p_cw_vec, observe_vec, res_p_w);
+    solver.TrianglateIterative(q_wc_vec, p_wc_vec, observe_vec, res_p_w);
     std::cout << "TestTrianglateIterative :";
     std::cout << "set p_w is " << p_w.transpose() << ", res p_w is " << res_p_w.transpose() << std::endl;
     return true;
